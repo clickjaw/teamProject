fb452debc132cc5e42ea99452a3837b006aae217
Jordan Hobson <hobsoncjordan@gmail.com>
2022-07-18 14:05:41 -0500

    Object constructor done

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ How can you best utilize these strengths in the execution of your project?
 In which professional competencies do you each want to develop greater strength?
 
 Knowing that every person in your team needs to understand all aspects of the project, how do you plan to approach the day-to-day work?
+
+
+
+
+       
---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict"
+// Main functions or actions
+// Create an object that includes:
+// image
+// scenario
+// correct answer
+let questionPic = document.getElementById("question-pic");
+let qBox = document.getElementById("qBox");
+
+function Question(q, answer, image){
+    this.q = q;
+    this.correctAnswer = answer;
+    this.image = image;
+}
+let allObjects = [];
+let q1 = new Question("You're in a cave and hear large footsteps approaching. What do you do?", "hide", "./assets/teamRocket.jpg");
+allObjects.push(q1);
+function pickQuestion(){
+    let rando = Math.floor(Math.random()*allObjects.length);
+    let pickedQuestion = allObjects[rando];
+    console.log(pickedQuestion.q);
+    console.log(pickedQuestion.correctAnswer);
+    questionPic.setAttribute("src", pickedQuestion.image);
+    questionPic.setAttribute("alt", "Cave");
+}
+pickQuestion();
